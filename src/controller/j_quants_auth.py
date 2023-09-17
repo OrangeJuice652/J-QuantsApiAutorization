e5a36203437ec6b828ee9cbe1d7f8012b3931e08
Azamino28Ki <49087471+Azamino28Ki@users.noreply.github.com>
2023-09-17 11:22:05 +0900

    Update j_quants_auth.py
    リフレッシュトークン取得処理を記載

--- src/controller/j_quants_auth.py
+++ src/controller/j_quants_auth.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 RESPONSE_OK = '200'
 RESPONSE_BAD_REQUEST = '400'
 RESPONSE_FORBIDDEN = '403'
@@ -28,4 +30,68 @@ class JQuantsApiIDTokenController():
     if id_token_fetcher_output.response_code == RESPONSE_OK:
         return self.id_token_fetcher_output.token
         # TODO: IDトークンが取得できなかった（response_code != RESPONSE_OK）のエラー処理
+
+@dataclass
+class TokenFetcherOutput():
+    response_code: str
+    response_message: str
+    token: str
+
+from abc import ABC, abstractmethod
+import requests
+import json
+
+JQUANTS_API_URI = 'https://api.jquants.com/v1' 
+REFRESH_TOKEN_URL = '/token/auth_user'
+
+class IRefreshTockenFetcher(ABC):
+    @abstractmethod
+    def __init__(
+        self,
+        mail_address: str,
+        password: str,
+    ):
+        pass
+
+    @abstractmethod
+    def fetch() -> TokenFetcherOutput:
+        pass
+
+class RefreshTokenFetcher(IRefreshTokenFetcher):
+    def __init__(
+        self,
+        mail_address: str,
+        password: str,
+    ):
+        self.mail_address = mail_address
+        self.password = password
+
+    def fetch() -> TokenFetcherOutput:
+        request_body = {
+            'mailaddress': self.mail_address,
+            'password': self.password,
+        }
+        response = requests.post(
+            JQUANTS_API_URI+REFRESH_TOKEN_URL,
+            data=json.dumps(request_body),
+        )
+        return TokenFetcherOutput(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
