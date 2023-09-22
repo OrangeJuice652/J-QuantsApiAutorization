a61409d09de7085cf60696be815f53c6fc2f74fe
Takuro Suetake <PassyonLips@outlook.jp>
2023-09-22 21:17:15 +0900

    JQuantsApiIDTokenController初期化テストを作成

---
+++ tests/controller/test_j_quants_auth.py
@@ -0,0 +1,33 @@
+import unittest
+from j_quants import JQuantsApiIDTokenController
+from j_quants.usecase import IIDTokenFetcher, IRefreshTokenFetcher
+
+class MockIDTokenFetcher(IIDTokenFetcher):
+    pass
+
+
+class MockRefreshTokenFetcher(IRefreshTokenFetcher):
+    pass
+
+
+class TestJQuantsApiIDTokenController(unittest.TestCase):
+    def setUp(self):
+        self.j_quants_api_id_token_controller = JQuantsApiIDTokenController(
+            MockRefreshTokenFetcher(),
+            MockIDTokenFetcher(),
+        )
+    
+    def test_init(self):
+        """JQuantsApiIDTokenControllerの初期化（__init__()）テスト
+        """
+        with self.subTest('refresh_token_fetcherの初期化'):
+            self.assertIsInstance(
+                self.j_quants_api_id_token_controller.refresh_token_fetcher,
+                IRefreshTokenFetcher,
+            )
+        
+        with self.subTest('id_token_fetcherの初期化'):
+            self.assertIsInstance(
+                self.j_quants_api_id_token_controller.id_token_fetcher,
+                IIDTokenFetcher,
+            )
